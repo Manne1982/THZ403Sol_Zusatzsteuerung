e3486bc988f9c3d5ba988f9ee027676d6f643373
Manfred R <41576281+Manne1982@users.noreply.github.com>
2022-08-04 22:02:53 +0200

    TSensor class updated

--- include/Klassen_HZS.cpp
+++ include/Klassen_HZS.cpp
@@ -1,9 +1,17 @@
 #include "Klassen_HZS.h"
+#define BGTDEBUG true
 
-TSensor::TSensor(byte * Address)
+TSensor::TSensor(OneWire * ds, byte * Address):
+TempMin(-55.0),
+TempMax(125.0),
+StartMeasureTime(0),
+NewValue(false)
 {
     addr = new byte[8];
     SType = new char[10];
+    Name = new char[15];
+    Source = ds;
+    strcpy(Name, "unnamed");
     for(int i = 0; i < 8; i++)
     {
         addr[i]= Address[i];
@@ -23,4 +31,69 @@ TSensor::TSensor(byte * Address)
             strcpy(SType, "Type NA");
             break;
     } 
+}
+TSensor::TSensor(OneWire * ds):
+TempMin(-55.0),
+TempMax(125.0),
+StartMeasureTime(0),
+NewValue(false)
+{
+    addr = new byte[8];
+    SType = new char[10];
+    Name = new char[15];
+    Source = ds;
+    strcpy(Name, "unnamed");
+    addr[0] = 0x00;
+}
+TSensor::~TSensor()
+{
+    delete[] addr;
+    delete[] SType;
+    delete[] Name;
+}
+bool TSensor::SensSearch()
+{
+    if(addr[0]!= 0x00)
+    {
+        #ifdef BGTDEBUG
+            Serial.println("Only new Object without Address could use for search!");
+        #endif
+        return false;
+    }
+    if(!Source->search(addr))
+    {
+        Source->reset_search();
+        delay(250);
+        #ifdef BGTDEBUG
+            Serial.println("No more addresses.");
+        #endif
+
+        return false;
+    }
+    else
+    {
+        if (OneWire::crc8(addr, 7) != addr[7]) {
+            #ifdef BGTDEBUG
+                Serial.print("ROM =");
+                for(int i = 0; i < 8; i++) {
+                    Serial.write(' ');
+                    Serial.print(addr[i], HEX);
+                    Serial.println();
+                }
+                Serial.print("CRC is not valid!");
+            #endif
+            return false;
+        }
+    }
+    return true;
+}
+void TSensor::startConversion()
+{
+    if(addr[0]==0)
+    return;
+    
+    Source->reset();
+    Source->select(addr);
+    Source->write(0x44, 1);        // start conversion, with parasite power on at the end
+    StartMeasureTime = millis();
 }
--- include/Klassen_HZS.h
+++ include/Klassen_HZS.h
@@ -7,13 +7,21 @@
 class TSensor
 {
     public:
-        TSensor(byte * Address);
+        TSensor(OneWire * ds, byte * Address);
+        TSensor(OneWire * ds);
+        bool SensSearch();
+        void startConversion();
         ~TSensor();
     private:
         byte * data;
         byte * addr;
         char * Name;
         char * SType;   //Addr[0] --> 0x10 = DS18S20; 0x28 = DS18B20; 0x22 = DS1822
+        float TempMin;
+        float TempMax;
+        unsigned long StartMeasureTime;
+        bool NewValue;
+        OneWire * Source;
 };
 
 
