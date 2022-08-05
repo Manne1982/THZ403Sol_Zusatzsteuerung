77038c19441a63ebe3917a5751c8ad906424edd3
Manfred R <41576281+Manne1982@users.noreply.github.com>
2022-08-05 14:13:42 +0200

    TSensor-Class finish for testing

--- include/Klassen_HZS.cpp
+++ include/Klassen_HZS.cpp
@@ -1,5 +1,5 @@
 #include "Klassen_HZS.h"
-#define BGTDEBUG true
+#define BGTDEBUG
 
 TSensor::TSensor(OneWire * ds, byte * Address):
 TempMin(-55.0),
@@ -7,11 +7,13 @@ TempMax(125.0),
 StartMeasureTime(0),
 NewValue(false)
 {
+    data = new byte[11];
     addr = new byte[8];
     SType = new char[10];
     Name = new char[15];
     Source = ds;
     strcpy(Name, "unnamed");
+    data[0] = 0;
     for(int i = 0; i < 8; i++)
     {
         addr[i]= Address[i];
@@ -38,12 +40,14 @@ TempMax(125.0),
 StartMeasureTime(0),
 NewValue(false)
 {
+    data = new byte[11];
     addr = new byte[8];
     SType = new char[10];
     Name = new char[15];
     Source = ds;
     strcpy(Name, "unnamed");
     addr[0] = 0x00;
+    data[0] = 0;
 }
 TSensor::~TSensor()
 {
@@ -91,9 +95,85 @@ void TSensor::startConversion()
 {
     if(addr[0]==0)
     return;
-    
+    NewValue = false;
     Source->reset();
     Source->select(addr);
     Source->write(0x44, 1);        // start conversion, with parasite power on at the end
     StartMeasureTime = millis();
+}
+bool TSensor::loop()
+{
+    if(StartMeasureTime==0)     //No Measurement started
+        return false;
+    data[10] = 0;
+    byte sensConfig = 0x03;
+    u_int16_t meassurementDelay;      
+    if(data[0] != 0)
+        sensConfig = (data[4] >> 5) & 0x03;     //Config on Bit 5 and Bit 6
+
+    switch (sensConfig)
+    {
+    case 0x00:
+        meassurementDelay = 94 + 50; //Datasheet 93,75ms + 50ms safety
+        break;
+    case 0x01:
+        meassurementDelay = 188 + 50; //Datasheet 187,5ms + 50ms safety
+        break;
+    case 0x02:
+        meassurementDelay = 375 + 50; //Datasheet 375ms + 50ms safety
+        break;    
+    default:
+        meassurementDelay = 750 + 50; //Datasheet 750ms + 50ms safety
+        break;
+    }
+    if(millis() < (StartMeasureTime + meassurementDelay)) //Delay time not expired
+        return false;
+    
+    data[10] = Source->reset();
+    Source->select(addr);    
+    Source->write(0xBE);         // Read Scratchpad
+    for (int i = 0; i < 9; i++) {           // we need 9 bytes
+        data[i] = Source->read();
+    }
+    data[9] = OneWire::crc8(data, 8);
+    #ifdef BGTDEBUG
+        Serial.println("Data (Byte 8 and 9 = CRC-check): ");
+        for (int i = 0; i < 11; i++) {           // we need 9 bytes
+            Serial.print(data[i], HEX);
+            Serial.print(" ");
+        }
+        Serial.println();
+    #endif
+    StartMeasureTime = 0;
+    NewValue = true;
+    return true;
+}
+float TSensor::getTempC()
+{
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (addr[0] == 0x10) {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10) {
+            // "count remain" gives full 12 bit resolution
+            raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+    } else {
+        byte cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+        //// default is 12 bit resolution, 750 ms conversion time
+    }
+    #ifdef BGTDEBUG
+        Serial.print("  Temperature = ");
+        Serial.print(((float)raw / 16.0));
+        Serial.print(" Celsius, ");
+    #endif
+
+    return (float)raw / 16.0;
 }
--- include/Klassen_HZS.h
+++ include/Klassen_HZS.h
@@ -9,8 +9,11 @@ class TSensor
     public:
         TSensor(OneWire * ds, byte * Address);
         TSensor(OneWire * ds);
-        bool SensSearch();
-        void startConversion();
+        bool SensSearch();      //Search new sensors, only if TSensor initiated without address (return value true = OK; false = no device or failure)
+        void startConversion(); //Measurement initiate 
+        bool loop();            //Have to start periotly to find out if there are a new measured value available if new Value available --> true
+        bool NewValueAvailable();   //If a new Value available that was not fetched --> true
+        float getTempC();           //Get saved Temperature as degree Celcius
         ~TSensor();
     private:
         byte * data;
