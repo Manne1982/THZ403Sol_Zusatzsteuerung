f3ba8bfefb9a1d9e0853bb5b1b53695e08a39f60
Manfred R <41576281+Manne1982@users.noreply.github.com>
2022-10-30 22:41:51 +0100

    Add struct for outputs & sensors

--- include/Klassen_HZS.h
+++ include/Klassen_HZS.h
@@ -1,34 +1,44 @@
 #ifndef Klassen_HZS
 #define Klassen_HZS
-
 #include <Arduino.h>
-#include <OneWire.h>
 
-class TSensor
-{
-    public:
-        TSensor(OneWire * ds, byte * Address);
-        TSensor(OneWire * ds);
-        bool SensSearch();      //Search new sensors, only if TSensor initiated without address (return value true = OK; false = no device or failure)
-        void startConversion(); //Measurement initiate 
-        bool loop();            //Have to start periotly to find out if there are a new measured value available if new Value available --> true
-        bool NewValueAvailable();   //If a new Value available that was not fetched --> true
-        float getTempC();           //Get saved Temperature as degree Celcius
-        void setName(String newName);
-        String getName();
-        ~TSensor();
-    private:
-        byte * data;
-        byte * addr;
-        char * Name;
-        char * SType;   //Addr[0] --> 0x10 = DS18S20; 0x28 = DS18B20; 0x22 = DS1822
-        float TempMin;
-        float TempMax;
-        unsigned long StartMeasureTime;
-        bool NewValue;
-        OneWire * Source;
+struct NWConfig {
+  //Einstellungen NW-Einstellungen WLAN
+  uint16 NW_Flags = 1; //See Enum NW_...
+  char WLAN_SSID[40] = "HeatingAC";
+  char WLAN_Password[70] = "";
+  //Einstellungen NW-Einstellungen MQTT
+  char MQTT_Server[50] = "192.168.178.5";
+  uint16 MQTT_Port = 1883;
+  char MQTT_Username[20] = "MQTT_User";
+  char MQTT_Password[70] = "12345";
+  char MQTT_fprint[70] = "";
+  char MQTT_rootpath[100] = "/Heating/Add_Control";
+  //Einstellungen NW-Einstellungen Netzwerk
+  char NW_NetzName[20] = "Add_Control";
+  char NW_IPAddress[17] = "192.168.178.10";
+  char NW_IPAddressEthernet[17] = "192.168.178.11";
+  char NW_SubMask[17] = "255.255.255.0";
+  char NW_Gateway[17] = "192.168.178.1";
+  char NW_DNS[17] = "192.168.178.1";
+  char NW_NTPServer[55] = "fritz.box";
+  char NW_NTPOffset = 0;
 };
 
+struct digital_Output{
+  char Name[15]="unnamed";
+  byte StartValue = 2; //0 = off; 1 = on; 2 = Auto
+  byte MCP = 0;     //0 = MCP one Address 38; 1 = MCP two Address 39
+  byte MCP_Pin_Auto = 0; //associated Pin in MCP-Modul. (1 - 16) for switch between manuel or automatic
+  byte MCP_Pin_OnOff = 0; //associated Pin in MCP-Modul. (1 - 16) for switch in manuel mode on or off
+  byte MQTTState = 0; //0 = MQTT control off; 1 = MQTT control on
+};
+
+struct TempSensor{
+  uint64 Address = 0; //8 Byte Address saved as unsigned int for easier comparison
+  char Name[15] = "unnamed"; //Name of Sensor
+  byte SensorState = 0; //0 = Sensor not found; 1 = Sensor found; 2 = MQTT enabled for found sensor
+};
 
 
 #include "Klassen_HZS.cpp"
